dd118e4d6975b0447f8f73ba3ee179bc14b2ab28
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-08-29 18:28:57 +0400

    Did the basic task

--- Sprint3/ViewController.swift
+++ Sprint3/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var clickButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabel.text = "Значение счётчика: \(count)"
+        
     }
+    
+    @IBAction func clickCounterButton(_ sender: Any) {
 
-
+        count += 1
+        counterLabel.text = "Значение счётчика: \(count)"
+    }
+    
 }
 
